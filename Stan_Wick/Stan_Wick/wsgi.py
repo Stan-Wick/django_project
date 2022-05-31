7f7e8a6314db22d44d0c2c6d1f512608ec7a7a77
Stan-Wick <105384071+Stan-Wick@users.noreply.github.com>
2022-05-31 15:14:02 +0100

    Add files via upload

---
+++ Stan_Wick/Stan_Wick/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Stan_Wick project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Stan_Wick.settings')
+
+application = get_wsgi_application()
